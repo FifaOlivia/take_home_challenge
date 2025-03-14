53f3ebe84d06cfed0eeaa3dc3d04ef001bff1c65
FifaOlivia <felicite.agoinon@epitech.eu>
2025-03-14 22:27:13 +0100

    [ADD]:add index js pour recuperer les données Excel utilisant stdin

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const getDataFromExcel = require("./getDataFromExcel");
 
-const result = getDataFromExcel("./features/fichier_30000_lignes.xlsx");
-console.log(result);
+process.stdin.on("data", (data) => {
+  const filePath = data.toString().trim();
+
+  if (filePath) {
+    try {
+      const result = getDataFromExcel(filePath);
+     
+    } catch (error) {
+      console.error(`Erreur lors de la lecture du fichier : ${error.message}`);
+    }
+  } else {
+    console.error("Aucun chemin de fichier fourni.");
+  }
+});
+
+// Écouter les événements de fin de stdin
+process.stdin.on("end", () => {
+  console.log("Fin de l'entrée standard.");
+});
